58343bba9c6b998fbfab58caacb2f8d572a14a4d
A rashid <coxringcox@gmail.com>
2026-05-21 23:51:39 +0600

    modifiy

---
+++ drive_nest/src/app/api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Vercel!");
+});
+
+app.get("/about", (req, res) => {
+  res.json({
+    message: "About Route",
+  });
+});
+
+module.exports = app;
